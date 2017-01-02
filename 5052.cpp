f793e456197d3c1827b1155cbafba68bfbe08a13
gwangjin <wgj2050@naver.com>
2017-01-02 17:13:06 +0900

    11055, 5052

---
+++ 5052.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	
+	int t;
+	cin >> t;
+	while (t--) {
+		string arr[10000];
+		int n;
+		cin >> n;
+		int ckk = 0;
+		for (int i = 0; i < n; i++) {
+			cin >> arr[i];
+		}
+		sort(arr, arr + n);
+		for (int i = 0; i < n; i++) {
+			int size_ = arr[i].length();
+			int ck = 0;
+
+			for (int k = i + 1; !ck&&k < n; k++) {
+				for (int j = 0; j < size_; j++) {
+					if (arr[i][j] != arr[k][j]) {
+						ck = 1;
+						break;
+					}
+				}
+				if (!ck) {
+					cout << "NO" << endl;
+					ckk = 1;
+					break;
+				}
+			}
+			if (ckk) break;
+		}
+		if (!ckk) cout << "YES" << endl;
+
+	}
+}
