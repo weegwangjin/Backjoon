c5c8ea0083e8f2f98e5614627f62ecc3b8b42f1f
wigwangjin <wgj2050@gmail.com>
2017-03-27 13:20:39 +0900

    binary indexed tree ( Fenwick Tree)

--- 7578.cpp
+++ 7578.cpp
@@ -1,42 +1,56 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <set>
+#include <iostream>
 #include <algorithm>
 #include <cstdio>
+#include <vector>
+
 using namespace std;
 
 int idx[1000001];
-int arr[500001];
+vector<pair<int,int> > vec(500001);
+vector<int> tree(500001,0);
+int n;
+
+long long sum(int i) {
+	long long ret = 0;
+	while (i > 0) {
+		ret += tree[i];
+		i -= (i& -i);
+	}
+	return ret;
+}
+
+void update(int i, int val)
+{
+	while (i <= n) {
+		tree[i] += val;
+		i += (i&-i);
+	}
+}
 int main()
 {
-	set<pair<int, int>> st;
-	int n;
+
 	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> vec[i].first;
+	}
+	for (int i = 1; i <= n; i++) {
+		int p;
+		cin >> p;
+		idx[p] = i;
+	}
+	for (int i = 1; i <= n; i++) {
+		vec[i].second = idx[vec[i].first];
+		vec[i].first = i;
+	}
+	long long ans = 0;
+	for (int i = n; i >=1; i--) {
+		update(vec[i].second, 1);
+		ans += sum(vec[i].second - 1);
+	}
+	cout << ans << endl;
 
 
-	for (int i = 1; i <= n; i++) scanf("%d", &arr[i]);
-	for (int i = 1; i <= n; i++) {
-		int k;
-		cin >> k;
-		idx[k] = i;
-	}
-	for (int i = 1; i <= n; i++) arr[i] = idx[arr[i]];
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (j<i && arr[j]> arr[i]) {
-				if (st.find({ i,j }) == st.end()) {
-					st.insert(make_pair(i, j));
-					st.insert(make_pair(j, i));
-				}
-			}
-			if (j > i && arr[j] < arr[i]) {
-				if (st.find({ i,j }) == st.end()) {
-					st.insert(make_pair(i, j));
-					st.insert(make_pair(j, i));
-				}
-			}
-		}
-	}
-	cout << st.size() / 2 << endl;
 }
