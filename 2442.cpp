53dc0d0090b1890d56008acb8615742cb0cca97d
wigwangjin <wgj2050@gmail.com>
2017-03-27 16:40:54 +0900

    17.03.27

---
+++ 2442.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int size_ = 2 * n - 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j <= size_; j++) {
+			if (j <= (size_ + 1) / 2 + i && j >= (size_ + 1) / 2 - i) {
+				cout << "*";
+			}
+			else if(j< (size_+1)/2 -i) cout << " ";
+		}
+		cout << endl;
+	}
+}
